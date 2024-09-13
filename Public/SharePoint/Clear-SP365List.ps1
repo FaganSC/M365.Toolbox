d1aed18e26c5e788a23e046f1d198f8c82281155
Shawn Fagan <shawn@fagan.me>
2024-09-12 21:02:06 -0400

    Initial Commit

---
+++ Public/SharePoint/Clear-SP365List.ps1
@@ -0,0 +1,26 @@
+function Clear-SP365List {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        $ListName
+    )
+    try {
+        $deletedCounter = 0
+        $itemTotal = (Get-PnPList -Identity $listName).ItemCount
+        do {
+            Get-PnPListItem -List $listName -PageSize 4500 | ForEach-Object {
+                $percent = "{0:n1}" -f (($deletedCounter / $itemTotal) * 100)
+                Write-Progress -Activity "Truncating SharePoint List: $listName (Items: $itemTotal)" -Status "$percent% Complete:" -PercentComplete $percent
+                Remove-PnPListItem -List $listName -Identity $_.Id -Force
+                $deletedCounter++
+            }
+        } until ((Get-PnPList -Identity $listName).ItemCount -eq 0)
+    }
+    catch {
+        if($_.Exception.Message -eq "The current connection holds no SharePoint context. Please use one of the Connect-PnPOnline commands which uses the -Url argument to connect."){
+            Write-Error "The current connection holds no SharePoint context. Please use one of the Connect-SP365 commands which uses the -Url argument to connect."
+        } else {
+            Write-Error $_.Exception.Message
+        }
+    }
+}
